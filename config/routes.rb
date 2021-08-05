d178f1852fa31b0c0efd515a15c017845261ad76
StevenDRiggs <sfriggs21@gmail.com>
2021-08-05 15:07:47 -0600

    corrected blog_entries#show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :blog_entries, except: [:show, :update, :destroy]
 
   post '/admin_login', to: 'users#admin_login'
-  get '/blog_entries/:slug', to: 'blog_entries#show'
+  get '/blog_entries/:slug', to: 'blog_entries#show', as: 'blog_entry'
   patch '/blog_entries/:slug', to: 'blog_entries#update'
   delete '/blog_entries/:slug', to: 'blog_entries#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
